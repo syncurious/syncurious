7c995d51df25bc41cd56f8767c2027420f33a52c
mubbashirhussain <mubbashirhhussain@gmail.com>
2025-02-18 21:06:50 +0500

    feat: add Header and Container components for layout structure

---
+++ src/components/section/container/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface ContainerProps {
+  children: React.ReactNode;
+  className?: string;
+  style?: React.CSSProperties;
+  innerClassName?: string;
+  innerStyle?: React.CSSProperties;
+}
+
+// temp
+
+const Container: React.FC<any> = (props) => {
+  const { children, className, style, innerStyle, innerClassName } = props;
+  return (
+    <div
+      style={style}
+      className={`w-full bg-background flex justify-center items-center ${className}`}
+    >
+      <div
+        style={innerStyle}
+        className={`w-full max-w-[1440px] px-3 relative ${innerClassName}`}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Container;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,7 @@ export default {
   theme: {
     extend: {
       colors: {
+        primary: "var(--primary)",
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
