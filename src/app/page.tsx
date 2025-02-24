c7b97e5ef7b1ff9d1a256097256b1b126936f45a
syncurious <syncurious@gmail.com>
2025-02-24 20:50:33 +0500

    feat: integrate ShpareBackgound component with advanced 3D effects and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import Header from "@/components/section/header";
 import Image from "next/image";
 import backgroundImage from "@/assets/images/moon-bg-dark.png";
 import Container from "@/components/section/container";
+import ShpareBackgound from "@/lib/three/shpareBackgound";
 
 export default function Home() {
   return (
     <div className="">
       <Container>
         <Header />
-        <Image src={backgroundImage} alt="background image" priority />
+        <ShpareBackgound />
+        {/* <Image src={backgroundImage} alt="background image" priority /> */}
       </Container>
     </div>
   );
--- src/lib/three/shpareBackgound.tsx
+++ src/lib/three/shpareBackgound.tsx
@@ -1,16 +1,52 @@
-'use client';
-import { Canvas } from '@react-three/fiber';
+"use client";
+import { Environment, Lightformer } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
 export default function ShpareBackgound() {
   return (
-    <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '50vh', zIndex: -1 }}>
-      <Canvas>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
-        <mesh>
-          <sphereGeometry args={[1, 32, 32]} />
-          <meshStandardMaterial color="#00ff00" />
+    <div
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "50vh",
+        zIndex: -1,
+      }}
+    >
+      <Canvas
+        flat
+        shadows
+        dpr={[1, 1.5]}
+        gl={{ antialias: false }}
+        camera={{ position: [50, 50, 30], fov: 17.5, near: 0, far: 40 }}
+      >
+        <color attach="background" args={["#171717"]} />
+        <mesh castShadow receiveShadow>
+          <sphereGeometry args={[2, 128, 128]} />
+          <meshStandardMaterial
+            color="#00ff00"
+            roughness={0.2}
+            metalness={0.5}
+          />
         </mesh>
+        <Environment resolution={256}>
+          <group rotation={[-Math.PI / 3, 0, 1]}>
+            {/* <Lightformer form="circle" intensity={100} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={2} /> */}
+            {/* <Lightformer form="circle" intensity={2} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={2} /> */}
+            {/* <Lightformer form="circle" intensity={2} rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={2} /> */}
+            <Lightformer
+              form="circle"
+              color="#171717"
+              intensity={100}
+              rotation-y={-Math.PI / 2}
+              position={[7, 7, 0]}
+              scale={15}
+            />
+            {/* <Lightformer form="ring" color="#171717" intensity={1000} onUpdate={(self) => self.lookAt(0, 0, 0)} position={[10, 10, 0]} scale={15} /> */}
+          </group>
+        </Environment>
+        {/* <Effects /> */}
       </Canvas>
     </div>
   );
