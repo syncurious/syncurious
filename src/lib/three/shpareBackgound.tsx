fa3690c391f72925a49350caf2e18d82516ef5d0
syncurious <syncurious@gmail.com>
2025-03-04 00:09:11 +0500

    feat: implement new features for enhanced user experience

--- src/lib/three/shpareBackgound.tsx
+++ src/lib/three/shpareBackgound.tsx
@@ -1,8 +1,30 @@
 "use client";
-import { Environment, Lightformer } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Leva, useControls } from "leva";
+import { PointLightHelper } from "three";
+import { useRef } from "react";
+import { PointLight } from "three";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
 
 export default function ShpareBackgound() {
+  const {
+    lightPosition1,
+    lightPosition2,
+    lightPosition3,
+    Lightintensity,
+    spherePosition,
+    sphereSize,
+    steps,
+  } = useControls({
+    lightPosition1: { value: { x: 0, y: 1.0, z: 2.5 }, step: 0.1 },
+    lightPosition2: { value: { x: 1.5, y: 0.5, z: 2.5 }, step: 0.1 },
+    lightPosition3: { value: { x: -1.5, y: 0.5, z: 2.5 }, step: 0.1 },
+    Lightintensity: { value: { x: 1, y: 1, z: 1 }, step: 0.1 },
+    spherePosition: { value: { x: 0, y: -3.0, z: 2.0 }, step: 0.1 },
+    sphereSize: { value: 3.5, min: 0.1, max: 100, step: 0.1 },
+    steps: { value: 512, min: 16, max: 1000, step: 0.1 },
+  });
+
   return (
     <div
       style={{
@@ -10,43 +32,40 @@ export default function ShpareBackgound() {
         top: 0,
         left: 0,
         width: "100%",
-        height: "50vh",
+        height: "100vh",
         zIndex: -1,
       }}
     >
-      <Canvas
-        flat
-        shadows
-        dpr={[1, 1.5]}
-        gl={{ antialias: false }}
-        camera={{ position: [50, 50, 30], fov: 17.5, near: 0, far: 40 }}
-      >
-        <color attach="background" args={["#171717"]} />
-        <mesh castShadow receiveShadow>
-          <sphereGeometry args={[2, 128, 128]} />
-          <meshStandardMaterial
-            color="#00ff00"
-            roughness={0.2}
-            metalness={0.5}
-          />
+      <Leva />
+      <Canvas>
+        <color attach="background" args={["#000"]} />
+        <pointLight
+          position={[lightPosition1.x, lightPosition1.y, lightPosition1.z]}
+          intensity={Lightintensity.x}
+          color={"#00ff00"}
+        />
+        <pointLight
+          position={[lightPosition2.x, lightPosition2.y, lightPosition2.z]}
+          intensity={Lightintensity.y}
+          color={"#00ff00"}
+        />
+        <pointLight
+          position={[lightPosition3.x, lightPosition3.y, lightPosition3.z]}
+          intensity={Lightintensity.z}
+          color={"#00ff00"}
+        />
+        <mesh position={[spherePosition.x, spherePosition.y, spherePosition.z]}>
+          <sphereGeometry args={[sphereSize, steps, steps]} />
+          <meshStandardMaterial color="#00ff00" />
         </mesh>
-        <Environment resolution={256}>
-          <group rotation={[-Math.PI / 3, 0, 1]}>
-            {/* <Lightformer form="circle" intensity={100} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={2} /> */}
-            {/* <Lightformer form="circle" intensity={2} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={2} /> */}
-            {/* <Lightformer form="circle" intensity={2} rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={2} /> */}
-            <Lightformer
-              form="circle"
-              color="#171717"
-              intensity={100}
-              rotation-y={-Math.PI / 2}
-              position={[7, 7, 0]}
-              scale={15}
-            />
-            {/* <Lightformer form="ring" color="#171717" intensity={1000} onUpdate={(self) => self.lookAt(0, 0, 0)} position={[10, 10, 0]} scale={15} /> */}
-          </group>
-        </Environment>
-        {/* <Effects /> */}
+        <EffectComposer>
+          <Bloom
+          
+            luminanceThreshold={-1}
+            luminanceSmoothing={0.3}
+            height={200}
+          />
+        </EffectComposer>
       </Canvas>
     </div>
   );
