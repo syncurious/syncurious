59aa38e99c45241ca0798d9c047b43756cf840c1
syncurious <syncurious@gmail.com>
2025-04-06 01:43:55 +0500

    feat: add custom 404 Not Found page with image and messaging

---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import Container from "@/components/section/container";
+
+import notFound from "@/assets/gif/404.gif"; // Adjust the path as necessary
+import Image from "next/image";
+import Heading from "@/components/base/heading";
+import Paragraph from "@/components/base/Paragraph";
+
+export default function NotFound() {
+  return (
+    <Container className="min-h-screen flex flex-col items-center justify-center">
+      <div className="text-center flex justify-center flex-col items-center">
+        <Image
+          src={notFound}  // Replace with your actual GIF path
+          alt="404 Not Found"
+          className="w-[50vh] aspect-square mb-4"
+        />
+        <h1 className="text-8xl font-bold text-white mb-2">404</h1>
+        <Paragraph className="text-gray-400 " level={3}>Page not found</Paragraph>
+      </div>
+    </Container>
+  );
+}
