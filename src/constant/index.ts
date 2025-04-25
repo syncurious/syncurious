d66f81885c42d1af49b36970440588e2665cdbc1
syncurious <syncurious@gmail.com>
2025-04-26 00:14:27 +0500

    Refactor portfolio sections for consistent height and update BlogCardData import path

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,4 +1,4 @@
-import { BlogCardData } from "../components/ui/ProjectsCard/index";
+import { BlogCardData } from "@/components/ui/BlogsCard";
 
 export const WhyChooseUsSectionsOptions = [
   {
@@ -39,6 +39,26 @@ export const HomeProjectCards: BlogCardData[] = [
 ];
 
 export const PortfolioWebsiteProjects: BlogCardData[] = [
+  {
+    name: "Syncurious",
+    title: "Mobile Application",
+    is_bottom_name: true,
+    is_link_disable: false,
+    link: "/",
+    description:
+      "Printers in the 1500s scrambled the words from Cicero's \"De Minibus Nongourmet Dolores'' after mixing the words in each sentence. They abbreviated the word Dolores, which means pain.",
+    image: "https://i.ytimg.com/vi/vDMyIZ2nsS0/maxresdefault.jpg",
+  },
+  {
+    name: "Syncurious",
+    title: "Mobile Application",
+    is_bottom_name: true,
+    is_link_disable: false,
+    link: "/",
+    description:
+      "Printers in the 1500s scrambled the words from Cicero's \"De Minibus Nongourmet Dolores'' after mixing the words in each sentence. They abbreviated the word Dolores, which means pain.",
+    image: "https://i.ytimg.com/vi/vDMyIZ2nsS0/maxresdefault.jpg",
+  },
   {
     name: "Syncurious",
     title: "Mobile Application",
